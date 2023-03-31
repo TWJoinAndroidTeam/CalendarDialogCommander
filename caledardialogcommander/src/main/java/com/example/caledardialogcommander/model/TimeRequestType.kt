7a5691f35b5892dc6d7c884a6f4038841debe4dd
Jimmy <linlin3253job@gmail.com>
2023-03-31 12:04:20 +0800

    feat(CalendarDialogUtil):fix logic

--- caledardialogcommander/src/main/java/com/example/caledardialogcommander/model/TimeRequestType.kt
+++ caledardialogcommander/src/main/java/com/example/caledardialogcommander/model/TimeRequestType.kt
@@ -7,24 +7,66 @@ interface TimeRequestType
 
 sealed class DateCalenderType(open val themeResId: Int) : TimeRequestType {
 
-    data class NormalCalender(override val themeResId: Int) : DateCalenderType(themeResId)
+    /**
+     * 一般的 DatePicker
+     * @param themeResId dialog style id
+     */
+    data class NormalCalender(override val themeResId: Int = 0) : DateCalenderType(themeResId)
 
-    data class StartNowCalender(val maxCalendar: Calendar? = null, override val themeResId: Int) : DateCalenderType(themeResId)
+    /**
+     * 開始時間為現在的DatePicker
+     * @param maxCalendar 日期區間最大值
+     * @param themeResId dialog style id
+     */
+    data class StartNowCalender(val maxCalendar: Calendar? = null, override val themeResId: Int = 0) : DateCalenderType(themeResId)
 
-    data class EndNowCalender(val minCalender: Calendar? = null, override val themeResId: Int) : DateCalenderType(themeResId)
+    /**
+     * 結束時間為現在的DatePicker
+     * @param minCalender 日期區間最小值
+     * @param themeResId dialog style id
+     */
+    data class EndNowCalender(val minCalender: Calendar? = null, override val themeResId: Int = 0) : DateCalenderType(themeResId)
 
+    /**
+     * 完全自定義區間的 DatePicker
+     * @param minCalender 日期區間最小值
+     * @param maxCalendar 日期區間最大值
+     * @param defaultCalender 當前日期
+     * @param themeResId dialog style id
+     */
     data class CustomRangeCalender(val minCalender: Calendar? = null, val maxCalendar: Calendar? = null, val defaultCalender: Calendar, override val themeResId: Int) : DateCalenderType(themeResId)
 }
 
 
 sealed class TimePickerType(open val is24Hours: Boolean, open val themeResId: Int) : TimeRequestType {
 
-    data class NormalTimePicker(override val is24Hours: Boolean, override val themeResId: Int) : TimePickerType(is24Hours, themeResId)
+    /**
+     * 一般的timePicker
+     * @param themeResId dialog style id
+     */
+    data class NormalTimePicker(override val is24Hours: Boolean, override val themeResId: Int = 0) : TimePickerType(is24Hours, themeResId)
 
-    data class StartOrBeforeNowCustomTimePicker(val minuteRange: Int, override val is24Hours: Boolean, override val themeResId: Int) : TimePickerType(is24Hours, themeResId)
+    /**
+     * 開始時間為現在的timePicker
+     * @param minuteRange 分鐘區間最大值
+     * @param themeResId dialog style id
+     */
+    data class StartNowCustomTimePicker(val minuteRange: Int, override val is24Hours: Boolean, override val themeResId: Int = 0) : TimePickerType(is24Hours, themeResId)
+
+    /**
+     * 結束時間為現在的timePicker
+     * @param minuteRange 分鐘區間最小值
+     * @param themeResId dialog style id
+     */
+    data class EndNowCustomTimePicker(val minuteRange: Int, override val is24Hours: Boolean, override val themeResId: Int = 0) : TimePickerType(is24Hours, themeResId)
 
     /**
      * 完全自定義區間的 TimePicker
+     * @param defaultHour 預設當前小時
+     * @param defaultMinute 預設當前分鐘
+     * @param minMinuteRange 分鐘區間最小值
+     * @param maxMinuteRange 分鐘區間最大值
+     * @param themeResId dialog style id
      */
     data class CustomRangeTimePicker(
         val defaultHour: Int,
@@ -32,6 +74,6 @@ sealed class TimePickerType(open val is24Hours: Boolean, open val themeResId: In
         val minMinuteRange: Int? = null,
         val maxMinuteRange: Int? = null,
         override val is24Hours: Boolean,
-        override val themeResId: Int
+        override val themeResId: Int = 0
     ) : TimePickerType(is24Hours, themeResId)
 }
